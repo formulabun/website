3bf27f27bd9e8ecf46e392a4b190c39f2ede7187
Fl_GUI <flor.guilini@hotmail.com>
2021-07-11 20:32:55 +0200

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import App from 'next/app';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import theme from '../config';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 10px;
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+    margin: 0;
+  }
+
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  }
+`;
+
+class FormulabunApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+        />
+        <Component {...pageProps} />
+      </>
+    );
+  }
+}
+
+export default FormulabunApp;
