01cb72dfece496690d9a1835529a3ec326e2a843
Fl_GUI <flor.guilini@hotmail.com>
2021-07-12 19:22:03 +0200

    added index page with semantic

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 import styled from 'styled-components';
+import 'semantic-ui-css/semantic.min.css'
+
+
+import { Container, Header } from 'semantic-ui-react';
+import Image from 'next/image';
+
 
 const Title = styled.h1`
   font-size: 3rem;
@@ -8,12 +14,14 @@ const Title = styled.h1`
 `;
 
 const Index = () => (
-  <>
-    <Head>
-      <title>Hello World</title>
-    </Head>
-    <Title>Hello World</Title>
-  </>
+    <Container>
+    <Header as="h1"> Formula bun </Header>
+      <Header> gaming </Header>
+      <Image src="/images/fastbun.png" width={1015} height={293}/>
+      <p>
+        <a href={`srb2kart://ip/${process.env.REACT_APP_SERVER}`}> join formula bun </a>
+      </p>
+    </Container>
 );
 
 export default Index;
